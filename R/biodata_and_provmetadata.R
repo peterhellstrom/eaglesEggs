bdd058eedea6470f54c18b5503133e35250895a7
peterhellstrom <peter.hellstrom@nrm.se>
2024-09-12 09:41:24 +0200

    added more metadata functions

--- R/biodata_and_provmetadata.R
+++ R/biodata_and_provmetadata.R
@@ -1,7 +1,72 @@
-# Construct WTSE provmetadata and biodata ----
 #' Title
 #'
-#' @param dsn 
+#' @param path
+#' @param sheet
+#' @param include_labcode
+#'
+#' @return
+#' @export
+#'
+#' @examples
+wtse_sites_eggs <- function(
+    path = "eggs.xlsx",
+    sheet = "Eggs",
+    include_labcode = FALSE
+) {
+
+  eggs <- readxl::read_xlsx(path, sheet) |>
+    dplyr::select(
+      PROV_KOD_ORIGINAL = `Acc-nr`,
+      PROV_KOD_LABB = analysnr_Clc,
+      PROVPLATS_ANALYSMALL = LKD
+    ) |>
+    dplyr::filter(
+      !is.na(PROV_KOD_ORIGINAL) & PROV_KOD_ORIGINAL != "kull"
+    )
+
+  if (include_labcode) {
+    out <- eggs |>
+      dplyr::select(
+        PROV_KOD_ORIGINAL,
+        PROV_KOD_LABB,
+        PROVPLATS_ANALYSMALL
+      )
+  } else {
+    out <- eggs |>
+      dplyr::select(
+        PROV_KOD_ORIGINAL,
+        PROVPLATS_ANALYSMALL
+      )
+  }
+  out |>
+    dplyr::distinct() |>
+    dplyr::mutate(
+      GENUS = "Haliaeetus albicilla"
+    )
+}
+
+#' Title
+#'
+#' @return
+#' @export
+#'
+#' @examples
+wtse_provdata_biota <- function() {
+  wtse_sites_eggs() |>
+    dplyr::select(PROV_KOD_ORIGINAL) |>
+    dplyr::distinct() |>
+    dplyr::mutate(
+      ART = "Havsorn",
+      DYNTAXA_TAXON_ID = "100067",
+      KON = "X",
+      ANTAL = 1,
+      KOMMENTAR_PROV = NA_character_
+    )
+}
+
+#' Title
+#'
+#' @param dsn
 #'
 #' @return
 #' @export
@@ -9,12 +74,12 @@
 #' @examples
 wtse_sites_provmetadata <- function(
     dsn = "Havsorn_Data") {
-  eagles::wtse_sites(odbc_name = dsn, add_monitoring = FALSE) |> 
-    dplyr::select(Region, Lokalkod, Lan) |> 
+  eagles::wtse_sites(odbc_name = dsn, add_monitoring = FALSE) |>
+    dplyr::select(Region, Lokalkod, Lan) |>
     dplyr::mutate(
       PROVDATA_TYP = "BIOTA",
       # This is the default value, but has to be changed
-      # depending on time (pre-1989 not NMO) and space 
+      # depending on time (pre-1989 not NMO) and space
       # (LST Y perhaps RMO, Projekt Havsörn sites not officially included
       # in the NMO programme)
       PROVTAG_SYFTE = "NMO",
@@ -33,10 +98,10 @@ wtse_sites_provmetadata <- function(
       PLATTFORM = "SAKNAS",
       PROVTAG_MET = "Aggplockning",
       DIREKT_BEHA = "KYLT"
-    ) |> 
+    ) |>
     dplyr::select(
       -Region, -Lan
-    ) |> 
+    ) |>
     dplyr::rename(
       PROVPLATS_ANALYSMALL = Lokalkod
     )
@@ -44,23 +109,23 @@ wtse_sites_provmetadata <- function(
 
 #' Title
 #'
-#' @param path 
-#' @param sheet 
-#' @param length_unit 
+#' @param path
+#' @param sheet
+#' @param length_unit
 #'
 #' @return
 #' @export
 #'
 #' @examples
-eggs_from_excel <- function(
+wtse_eggs_from_excel <- function(
     path = "eggs.xlsx",
     sheet = "Eggs",
     length_unit = c("cm", "mm")
 ) {
-  
+
   length_unit <- match.arg(length_unit, c("cm", "mm"))
-  
-  out <- readxl::read_xlsx(path, sheet) |> 
+
+  out <- readxl::read_xlsx(path, sheet) |>
     dplyr::select(
       PROV_KOD_ORIGINAL = `Acc-nr`,
       discovery_date_start = ins.datum,
@@ -71,17 +136,17 @@ eggs_from_excel <- function(
       egg_membrane_thickness = `hinna (mm)`,
       embryo_weight = `fostervikt (g)`,
       embryo_length = `fosterlängd (mm)`
-    ) |> 
+    ) |>
     dplyr::filter(
       !is.na(PROV_KOD_ORIGINAL) & PROV_KOD_ORIGINAL != "kull"
-    ) |> 
+    ) |>
     dplyr::mutate(
       discovery_date_start = janitor::convert_to_date(
-        discovery_date_start, 
+        discovery_date_start,
         string_conversion_failure = "warning"
       )
-    ) |> 
-    eaglesEggs::fix_null_strings() |> 
+    ) |>
+    eaglesEggs::fix_null_strings() |>
     dplyr::rename(
       PROVTAG_DAT = discovery_date_start,
       TOTV = specimen_weight,
@@ -90,7 +155,7 @@ eggs_from_excel <- function(
       SKLV = shell_weight,
       # SKLI = shell_index,
       FOSL = embryo_length
-    ) |> 
+    ) |>
     dplyr::mutate(
       # How should SEX/KON be treated, this is not even a valid
       # parameter if an egg is not fertilized?
@@ -100,17 +165,17 @@ eggs_from_excel <- function(
       # Assume no pools to start with!
       ANTAL_DAGAR = 1
     )
-  
+
   if (length_unit == "cm") {
-    out <- out |> 
+    out <- out |>
       dplyr::mutate(
-        # Convert from mm to cm 
+        # Convert from mm to cm
         # (as this is the unit we have specified in our codelist!)
         TOTL = TOTL/10,
         BRED = BRED/10
       )
   }
-  
+
   out
 }
 
@@ -122,51 +187,51 @@ eggs_from_excel <- function(
 # put that commentary column?
 #' Title
 #'
-#' @param path 
-#' @param extended_types 
-#' @param query 
-#' @param length_unit 
+#' @param path
+#' @param extended_types
+#' @param query
+#' @param length_unit
 #'
 #' @return
 #' @export
 #'
 #' @examples
-eggs_from_esbase <- function(
+wtse_eggs_from_esbase <- function(
     path = "esb/esbase/esbase_dump.db",
     extended_types = TRUE,
     query = "SELECT * FROM v_egg WHERE species_id = 597",
     length_unit = c("cm", "mm")
 ) {
-  
+
   on.exit(DBI::dbDisconnect(con))
-  
+
   length_unit <- match.arg(length_unit, c("cm", "mm"))
-  
+
   con <- DBI::dbConnect(
-    RSQLite::SQLite(), 
-    path, 
+    RSQLite::SQLite(),
+    path,
     extended_types = extended_types
   )
-  
-  out <- DBI::dbGetQuery(con, query) |> 
-    eaglesEggs::fix_null_strings() |> 
+
+  out <- DBI::dbGetQuery(con, query) |>
+    eaglesEggs::fix_null_strings() |>
     dplyr::select(
-      accession_id, 
+      accession_id,
       discovery_date_start,
       species_id,
-      specimen_weight, 
-      starts_with("shell"), 
-      starts_with("egg"), 
+      specimen_weight,
+      starts_with("shell"),
+      starts_with("egg"),
       starts_with("embryo")
-    ) |> 
-    eaglesEggs::add_accnr() |> 
+    ) |>
+    eaglesEggs::add_accnr() |>
     dplyr::select(
-      -accession_id, 
+      -accession_id,
       -species_id,
       -embryo_weight,
       # perhaps turn embryo column into a text comment later
       -embryo
-    ) |> 
+    ) |>
     dplyr::rename(
       PROVTAG_DAT = discovery_date_start,
       TOTV = specimen_weight,
@@ -175,7 +240,7 @@ eggs_from_esbase <- function(
       SKLV = shell_weight,
       SKLI = shell_index,
       FOSL = embryo_length
-    ) |> 
+    ) |>
     # Note that ORGAN has to be included
     # in analysdata (as we can have data from multiple tissues per accnr)
     dplyr::mutate(
@@ -187,16 +252,16 @@ eggs_from_esbase <- function(
       # Assume no pools to start with!
       ANTAL_DAGAR = 1
     )
-  
+
   if (length_unit == "cm") {
-    out <- out |> 
+    out <- out |>
       dplyr::mutate(
-        # Convert from mm to cm 
+        # Convert from mm to cm
         # (as this is the unit we have specified in our codelist!)
         TOTL = TOTL/10,
         BRED = BRED/10
       )
   }
-  
+
   out
 }
